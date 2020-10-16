320a25f4fbb11873acb5b173a35b70d854d11881
Louis Stevens <loustevenshere@gmail.com>
2020-10-16 12:15:28 -0400

    Finished callbacks, and closures(almost)

--- challenges/closure.js
+++ challenges/closure.js
@@ -16,9 +16,18 @@ function myFunction() {
 }
 myFunction();
 
-// Explanation: 
+// Explanation: Because of function scope, the variable internal is inside of the parent function of myFunction so it can access the variables inside of its parent function. 
+// It could not go from child function to parent function though
+
+
 
 
 /* Task 2: Counter */
 
 /* Create a function called `summation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+function counter(num) {
+  for(let i = 0;i<=num;i++)
+  return i++;
+ }
+
+ console.log(counter(5))
